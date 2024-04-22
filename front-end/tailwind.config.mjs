01757c8a4fcaf41af5be95c14dab892623ec91f2
shard <sh7user@gmail.com>
2024-04-23 01:01:24 +0200

    added basic index template, custom font

--- front-end/tailwind.config.mjs
+++ front-end/tailwind.config.mjs
@@ -5,11 +5,41 @@ const { nextui } = require("@nextui-org/react");
 export default {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx'
+		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				jersey: ["Jersey 25", "sans-serif"],
+			}
+		},
 	},
 	darkMode: "class",
-	plugins: [nextui()],
-}
+	plugins: [
+		nextui({
+			themes: {
+				light: {
+					colors: {
+						background: "#030913",
+						foreground: "#cfd6e6",
+						secondary: "#7096e7",
+						primary: {
+							foreground: "#cfd6e6",
+							DEFAULT: "#006FEE",
+						},
+					},
+				},
+				dark: {
+					colors: {
+						background: "#030913",
+						foreground: "#cfd6e6",
+						primary: {
+							foreground: "#cfd6e6",
+							DEFAULT: "#cfd6e6",
+						},
+					},
+				},
+			},
+		}),
+	],
+};
